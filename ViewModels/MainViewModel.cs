0c5a3c2473af4d6863c5dc4969051bdeea8e11e9
Никитин Павел Юрьевич <sayrom43@gmail.com>
2023-06-09 08:03:40 +0500

    Небольшие правки в MainView / MainViewModel

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -7,8 +7,27 @@ using System.Threading.Tasks;
 
 namespace Organizer.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : Conductor<object>
     {
+        private readonly IWindowManager WindowManager;
+
+        public MainViewModel(IWindowManager windowManager)
+        {
+            WindowManager = windowManager;
+        }
+
         public BindableCollection<BaseDocumentViewModel> Documents { get; set; }
+
+        protected async override void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+
+            Documents = new BindableCollection<BaseDocumentViewModel>();
+        }
+
+        public Task AddDocument()
+        {
+            return WindowManager.ShowDialogAsync(IoC.Get<DocumentViewModel>());
+        }
     }
 }
