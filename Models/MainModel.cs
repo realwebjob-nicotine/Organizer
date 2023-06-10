44dc96746a85d841787a67ee7df28e632175a763
Никитин Павел Юрьевич <sayrom43@gmail.com>
2023-06-10 18:23:26 +0500

    Добавлена модель для получения данных
    Отошел от ТЗ, оформил хранение данных в файле, в формате json.

--- Models/MainModel.cs
+++ Models/MainModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Organizer.Types;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,40 @@ namespace Organizer.Models
 {
     public class MainModel
     {
+        private string path = "data.json";
+
+        public void AddDocument(BaseDocument document)
+        {
+            var documents = ReadDocuments();
+            documents.Add(document);
+            WriteDocuments(documents);
+        }
+
+        public void DeleteDocument(BaseDocument document)
+        {
+            var documents = ReadDocuments();
+            documents.Remove(document);
+            WriteDocuments(documents);
+        }
+
+        public List<BaseDocument> ReadDocuments()
+        {
+            if (File.Exists(path))
+            {
+                var jsonString = File.ReadAllText(path);
+                return JsonConvert.DeserializeObject<List<BaseDocument>>(jsonString);
+            }
+            else
+            {
+                var documents = new List<BaseDocument>();
+                WriteDocuments(documents);
+                return documents;
+            }
+        }
+
+        public void WriteDocuments(List<BaseDocument> documents)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(documents));
+        }
     }
 }
