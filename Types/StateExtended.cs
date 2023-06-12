4e90c7c927db75eeebc3583a8334be538195b835
Никитин Павел Юрьевич <sayrom43@gmail.com>
2023-06-12 11:44:37 +0500

    fix Редактор задачи: решил вопрос с combobox (состояние)

---
+++ Types/StateExtended.cs
@@ -0,0 +1,14 @@
+﻿using Organizer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizer.Types
+{
+    public class StateExtended
+    {
+        public Enums.State State { get; set; }
+        public string Name { get; set; }
+    }
+}
--- ViewModels/TaskViewModel.cs
+++ ViewModels/TaskViewModel.cs
@@ -3,7 +3,9 @@ using Organizer.Models;
 using Organizer.Types;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,7 +33,12 @@ namespace Organizer.ViewModels
                 Id = document.Id;
                 Name = document.Name;
                 Description = document.Description;
-                State = document.State;
+                var tempState = (Enums.State)document.State;
+                SelectedState = new StateExtended()
+                {
+                    State = tempState,
+                    Name = GetEnumDescription(tempState)
+                };
 
                 NotifyOfPropertyChange(() => document);
             }
@@ -41,14 +48,22 @@ namespace Organizer.ViewModels
         public string Name { get; set; }
         public string Description { get; set; }
         public Type Type { get; set; }
-        public Enum State { get; set; }
+        public StateExtended SelectedState { get; set; }
         public Enums.WindowMode Mode { get; set; }
+
+        public BindableCollection<StateExtended> States { get; set; }
 
         protected override void OnViewLoaded(object view)
         {
             base.OnViewLoaded(view);
 
             GenerateId();
+            LoadStates();
+
+            if (Mode == Enums.WindowMode.Create)
+            {
+                SelectedState = States.FirstOrDefault();
+            }
         }
 
         public void Save()
@@ -59,7 +74,7 @@ namespace Organizer.ViewModels
                 Name = Name,
                 Description = Description,
                 Type = Enums.Type.Task,
-                State = State
+                State = SelectedState.State
             };
 
             //TODO: на рефакторинг, повторяющийся код можно вынести в сервис
@@ -87,6 +102,27 @@ namespace Organizer.ViewModels
                 Id = Model.GetMaxId();
                 Id++;
             }
+        }
+
+        private void LoadStates()
+        {
+            var tempStates = Enum.GetValues(typeof(Enums.State))
+                .Cast<Enums.State>()
+                .Select(s => new StateExtended() { State = s, Name = GetEnumDescription(s) });
+
+            States = new BindableCollection<StateExtended>(tempStates);
+        }
+
+        private string GetEnumDescription(Enum value)
+        {
+            // Get the Description attribute value for the enum value
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes.Length > 0)
+                return attributes[0].Description;
+            else
+                return value.ToString();
         }
     }
 }
